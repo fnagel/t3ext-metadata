4301de5ab114683858f9f64924475e01acde039d
Felix Nagel <info@felixnagel.com>
2014-06-02 22:21:48 +0200

    [BUGFIX] add missing exif data fields in database

---
+++ ext_tables.sql
@@ -0,0 +1,7 @@
+#
+# Table structure for table 'sys_file_metadata'
+#
+CREATE TABLE sys_file_metadata (
+	horizontal_resolution int(11) unsigned DEFAULT NULL,
+	vertical_resolution int(11) unsigned DEFAULT NULL,
+);
